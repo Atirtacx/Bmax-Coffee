3ca03358c16bbc631a2633d07db332a09ace2847
Atirtacx <anggasta.kusuma@gmail.com>
2025-05-04 22:31:51 +0800

    Adding menu sidebar javascript

--- Assets/Javascript/main.js
+++ Assets/Javascript/main.js
@@ -7,3 +7,10 @@ const navbarMenu = document.querySelector(".navbar_menu");
 document.querySelector("#hamburger-menu").onclick = () => {
   navbarMenu.classList.toggle("active");
 };
+
+const hamburger = document.querySelector("#hamburger-menu");
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarMenu.contains(e.target)) {
+    navbarMenu.classList.remove("active");
+  }
+});
